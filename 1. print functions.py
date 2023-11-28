accde206a99e5c30ef016d2f61667fb8c821b4d4
NjauSamuel <njaus602@gmail.com>
2023-11-29 02:27:11 +0300

    All these are my Udemy Python Projects I've been tackling for 100 days, I'm currently at day 2

---
+++ 1. print functions.py
@@ -0,0 +1,40 @@
+print("Day 1. Python printing function")
+
+
+# A back slash new line could help us in the implementation of multiple lines of print statements into a single one
+
+# And example
+print("Hello World!\nHello World\nHello World!")
+
+
+# String concatenation and Code intelligence, is possible through the '+' operator.
+# For example
+# For this part, the space could be added throught the help of another string at the middle i.e " "
+print("Hello" + "Njau")
+
+# Code intelligence referes to the way a ode editor is able to show you the errors that you get, e.g for vs code.
+
+
+# Code debugging
+# This referes to the removal of errors from a piece of code
+print("Day 1, string manipulation")
+print("string concatenation is done with the '+' sign")
+print('e.g. print("Hello" + " World")')
+print("New lines can be created with a backslash plus an n.")
+
+
+# The Python Input Function
+# It looks like this "input(prompt)"
+
+# an example use looks like
+input("What is your name?")
+
+print("Hello" + input("What is your name?"))
+
+name = input("What is your name?")
+print(len(name))
+
+# since t is called a variable, ot can vary
+
+
+# Challenge Switch the values between two variables
---
+++ 1.2 Switheroo variables.py
@@ -0,0 +1,17 @@
+# There are two variables, a and b from input
+a = input()
+b = input()
+
+# Don't change the code pieces above
+# The task is to switch the content in the two variables.
+# Below is my solution
+
+
+c = a
+a = b
+b = c
+
+# The code output will now be in the format below:
+
+print("a: " + a)
+print("b: " + b)
